a39e889b36dd00f9929ef39cf5afff58fefe748a
y41-L <yao.lin233@stu.emuhsd.org>
2017-05-30 20:57:56 -0700

    Delete jscript.js

--- jscript.js
+++
@@ -1,9 +0,0 @@
-var mn = $(".leftbut");
-
-$(window).scroll(function() {
-    if ($(this).scrollTop() > 300) {
-        mn.addClass("button-scrolled");
-    } else {
-        mn.removeClass("button-scrolled");
-    }
-});
